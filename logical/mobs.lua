42cf3ff6583b43be85447aa91dd3ab131d74b766
zFeikG <39036738+zfeikg@users.noreply.github.com>
2018-07-17 01:11:47 +0000

    :sparkles: Removed unnecessary functions.

--- logical/mobs.lua
+++ logical/mobs.lua
@@ -1,46 +1,5 @@
 
-function wildman_Interact(mob, player)
-	local id =	ModMgr:getMonsterIdByName("Wildman_tamed");
-	return TameAnimalInPanic_Interact(mob, player, 11204, id, 3);
+function Oldman_onJoin(mob, player)
+    local info = player:getNickname()..GetS(8025);
+    ClientCurGame:sendChat(info, 1);
 end
-
-function wildman_tamed_Interact(mob, player)
-	local itemid = player:getCurToolID()
-	if mob:getTamed() then
-		if  mob:getTamedOwnerID() == player:getUin() and (0 == mob:isBreedItem(itemid))  then
-			mob:setAISitting(not mob:getSitting())
-			return true
-		end
-	end
-end	
-
-
-function wildmanhunter_Interact(mob, player)
-	local id =	ModMgr:getMonsterIdByName("WildmanHunter_tamed");
-	return TameAnimalInPanic_Interact(mob, player, 11204, id, 3);
-end
-
-function wildmanhunter_tamed_Interact(mob, player)
-	local itemid = player:getCurToolID()
-	if mob:getTamed() then
-		if  mob:getTamedOwnerID() == player:getUin() and (0 == mob:isBreedItem(itemid))  then
-			mob:setAISitting(not mob:getSitting())
-			return true
-		end
-	end
-end	
-
-function littlewildman_Interact(mob, player)
-	local id =	ModMgr:getMonsterIdByName("LittleWildman_tamed");
-	return TameAnimalInPanic_Interact(mob, player, 11204, id, 3);
-end
-
-function littlewildman_tamed_Interact(mob, player)
-	local itemid = player:getCurToolID()
-	if mob:getTamed() then
-		if  mob:getTamedOwnerID() == player:getUin() and (0 == mob:isBreedItem(itemid))  then
-			mob:setAISitting(not mob:getSitting())
-			return true
-		end
-	end
-end	
